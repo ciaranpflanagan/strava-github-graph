443f5ea15e234225dc5818a9cd141ada9d7f4747
Ciaran Flanagan <ciaran@cicisoft.net>
2025-03-01 19:26:07 +0000

    Parsing athlete strava data

--- main.go
+++ main.go
@@ -1,35 +1,61 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
-    "net/http"
     "io/ioutil"
     "log"
+    "net/http"
+    "time"
 )
 
 type Activity struct {
-	Title string
-	Distance  int
-	Duration  int
+    Name       string
+    Distance   float64
+    MovingTime int       `json:"moving_time"`
+    StartDate  time.Time `json:"start_date"`
 }
 
-const accessToken = "token"
+const accessToken = ""
+const test = true
 
 func getActivities() {
-    url := "https://www.strava.com/api/v3/athlete/activities"
-    req, _ := http.NewRequest("GET", url, nil)
+    var body string
+    if test {
+        data, err := ioutil.ReadFile("./data/test.json")
+        if err != nil {
+            log.Fatal(err)
+        }
+        body = string(data)
+    } else {
+        url := "https://www.strava.com/api/v3/athlete/activities"
+        req, _ := http.NewRequest("GET", url, nil)
 
-    req.Header.Set("Authorization", "Bearer "+accessToken)
+        req.Header.Set("Authorization", "Bearer "+accessToken)
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer resp.Body.Close()
+
+        responseBody, _ := ioutil.ReadAll(resp.Body)
+        body = string(responseBody)
+    }
+
+    activities := saveActivities(body)
+    fmt.Println(activities)
+}
+
+func saveActivities(body string) []Activity {
+    var activities []Activity
+    err := json.Unmarshal([]byte(body), &activities)
     if err != nil {
         log.Fatal(err)
     }
-    defer resp.Body.Close()
 
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println(string(body)) // Raw JSON response
+    return activities
 }
 
 func main() {
